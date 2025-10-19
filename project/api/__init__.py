f7e22c83ccbcfea665bf2124562ed6ba43734e71
yinxie <yinxiehuang@gmail.com>
2025-10-19 22:21:49 +0800

    feat: 實作 BookCopy (館藏複本) 的 CRUD API

--- project/api/__init__.py
+++ project/api/__init__.py
@@ -1,7 +1,9 @@
 from flask_restx import Api
 from .books import book_ns
 from .libraries import library_ns
+from .copies import copy_ns
 
 def register_routes(api: Api):
     api.add_namespace(book_ns)
     api.add_namespace(library_ns)
+    api.add_namespace(copy_ns)
